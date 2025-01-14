9c2eddcb772508fdbf3a55278069faf56729a7cd
Alexis Manin <alexis.manin@geomatys.com>
2025-01-14 16:54:11 +0100

    chore(Build): Upgrade Spring Boot convention plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "crs-service"
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        // Required for Spring-Boot convention plugin and Bill Of Material (automatic dependency version management)
         maven("https://nexus.geomatys.com/repository/maven-public")
     }
 }
