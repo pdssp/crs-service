1753d52527c3ea223d4d94a4c3ef540eb6ca7f32
Alexis Manin <alexis.manin@geomatys.com>
2025-01-14 17:27:32 +0100

    chore(Git): add license information
    LGPL v3, copyright CNES / PDSSP

--- src/main/java/com/geomatys/crsservice/rest/CRSOperationController.java
+++ src/main/java/com/geomatys/crsservice/rest/CRSOperationController.java
@@ -1,3 +1,22 @@
+/*
+ * Planet CRS Registry - The coordinates reference system registry for solar bodies
+ * Copyright (C) 2025 - CNES (for PDSSP)
+ *
+ * This file is part of CRS Service.
+ *
+ * CRS Service is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License v3  as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CRS Service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License v3  for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License v3
+ * along with CRS Service.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.geomatys.crsservice.rest;
 
 import com.geomatys.crsservice.service.CrsOperationService;
